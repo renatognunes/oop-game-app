7d3689d8a0d4c76ec95f857f317127fbb2433a5b
Renato Guimaraes Nunes <renatognunes@hotmail.com>
2019-06-16 14:01:56 -0400

    Added classes and constructors

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,24 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+     constructor() {
+         this.missed = 0;
+         this.phrases = this.createPhrases();
+         this.activePhrase = null;
+     }
+     createPhrases = () => {
+        const phrase1 = {phrase: 'Life is good'};
+        const phrase2 = {phrase: 'Keep Working'};
+        const phrase3 = {phrase: 'You can do it'};
+        const phrase4 = {phrase: 'Have a nice day'};
+        const phrase5 = {phrase: 'Thank you'};
+        return [phrase1, phrase2, phrase3, phrase4, phrase5]
+     };
+
+     getRandomPhrase = () => {
+         const randomIndex = Math.floor(Math.random() * 5);
+         return this.phrases[randomIndex];
+     }
+ }
